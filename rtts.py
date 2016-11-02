5ce2a37425626bfbfdffa1efcec69218d4f74489
Sukyun Chung <chungsukyun@berkeley.edu>
2016-11-02 15:14:14 -0700

    committed

--- rtts.py
+++ rtts.py
@@ -12,13 +12,13 @@ if function_name == "run_ping":
     while line != "":
         hostnames += [line]
         line = hostname_file.readline().rstrip()
-    num_packets = sys.argv[3]
+    num_packets = sys.argv[3] + 1
     raw_ping_output_filename = sys.argv[4]
     aggregated_ping_output_filename = sys.argv[5]
     raw_ping_dict = {}
     aggregated_ping_dict = {}
     name = hostnames[0]
-    ls_output = subprocess.check_output("ping -c " + str(num_packets+1) + " " + name, shell = True).decode("utf-8")
+    ls_output = subprocess.check_output("ping -c " + num_packets + " " + name, shell = True).decode("utf-8")
     print ls_output
     rtt_list = []
 
