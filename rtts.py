72a9d939cb0b414f09948c29dd02818cf05aa7b6
Sukyun Chung <chungsukyun@berkeley.edu>
2016-11-02 15:11:27 -0700

    committed

--- rtts.py
+++ rtts.py
@@ -15,9 +15,14 @@ if function_name == "run_ping":
     num_packets = sys.argv[3]
     raw_ping_output_filename = sys.argv[4]
     aggregated_ping_output_filename = sys.argv[5]
-    for name in hostnames:
-        ls_output = subprocess.check_output("ping -c " + num_packets + " " + name, shell=True).decode("utf-8")
-        print ls_output
+    raw_ping_dict = {}
+    aggregated_ping_dict = {}
+    name = hostnames[0]
+    ls_output = subprocess.check_output("ping -c " + num_packets + " " + name, shell = True).decode("utf-8")
+    print ls_output
+    # for name in hostnames:
+    #     ls_output = subprocess.check_output("ping -c " + num_packets + " " + name, shell=True).decode("utf-8")
+    #     print ls_output
 
 elif function_name == "plot_median_rtt_cdf":
     pass
