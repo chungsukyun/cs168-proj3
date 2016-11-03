fb9bff7ada3efebc5917e91b9064dc1f18474feb
Sukyun Chung <chungsukyun@berkeley.edu>
2016-11-03 13:37:41 -0700

    committed

--- traceroute.py
+++ traceroute.py
@@ -11,9 +11,27 @@ function_name = sys.argv[1]
 def find_num_names(lst):
     i = 0
     for e in lst:
-        if e[:3] == "[AS":
+        if e[0] == "[":
             i += 1
     return i
+
+def find_index_string(lst, s):
+    i = 0
+    while i < len(lst):
+        e = lst[i]
+        if e[:len(s)]==s:
+            return i
+        i += 1
+    return ""
+
+def find_string(lst, s):
+    i = 0
+    while i < len(lst):
+        e = lst[i]
+        if e[:len(s)]==s:
+            return e
+        i += 1
+    return ""
 
 if function_name == "run_traceroute":
     hostnames = []
@@ -28,8 +46,8 @@ if function_name == "run_traceroute":
     traceroute_dict = {}
     timestamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")
     traceroute_dict["timestamp"] = timestamp
-    print traceroute_dict["timestamp"]
     for name in hostnames:
+        name_hops = []
         ls_output, err = subprocess.Popen(["traceroute", "-A", "-q", str(num_packets), name], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
         ls_output_lines = ls_output.splitlines()
         for line in ls_output_lines:
@@ -37,7 +55,31 @@ if function_name == "run_traceroute":
             if line[0] == "traceroute":
                 continue
             if find_num_names(line) == 0:
-                pass
+                name_hops += [[{"name": "None", "ip": "None", "ASN": "None"}]]
+            else:
+                entry = []
+                for i in range(find_num_names(line)):
+                    ASN = ""
+                    IP = ""
+                    Name = ""
+                    j = find_index_string(line, "[")
+                    if line[j][1] == "*":
+                        ASN = "None"
+                    else:
+                        ASN = line[j][3:(len(line[j])-1)]
+                    if line[j-1][1] == "*":
+                        IP = "None"
+                    else:
+                        IP = line[j-1][1:(len(line[j])-1)]
+                    if line[j-2][0] == "*":
+                        Name = "None"
+                    else:
+                        Name = line[j-2]
+                    line = line[j+1:]
+                    entry += [{"name": Name, "ip": IP, "ASN": ASN}]
+                name_hops += [entry]
+        traceroute_dict[name] = name_hops
+    print traceroute_dict
 
 
 elif function_name == "parse_traceroute":
