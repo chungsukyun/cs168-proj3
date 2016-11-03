a6fddaa6b3ca3e122becc9027ca15ffd45fcdf08
Sukyun Chung <chungsukyun@berkeley.edu>
2016-11-03 12:50:30 -0700

    committed

---
+++ traceroute.py
@@ -0,0 +1,26 @@
+import subprocess
+import json
+import sys
+import numpy
+import math
+
+function_name = sys.argv[1]
+
+if function_name = "run_traceroute":
+    hostnames = []
+    hostname_file_name = sys.argv[2]
+    hostname_file = open(hostname_file_name, 'r')
+    line = hostname_file.readline().rstrip()
+    while line != "":
+        hostnames += [line]
+        line = hostname_file.readline().rstrip()
+    num_packets = int(sys.argv[3])
+    output_filename = sys.argv[4]
+    for name in hostnames:
+        ls_output, err = subprocess.Popen(["traceroute", "-a", num_packets, name], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        print ls_output
+
+elif function_name = "parse_traceroute":
+    pass
+else:
+    print "The function you have called does not exist."
