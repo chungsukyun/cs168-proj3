b21e2126c603018e71d03e0d47456e8b662b3cf3
Sukyun Chung <chungsukyun@berkeley.edu>
2016-11-03 13:14:47 -0700

    committed

--- traceroute.py
+++ traceroute.py
@@ -3,8 +3,17 @@ import json
 import sys
 import numpy
 import math
+import time
+import datetime
 
 function_name = sys.argv[1]
+
+def find_num_names(lst):
+    i = 0
+    for e in lst:
+        if e[:3] == "[AS":
+            i += 1
+    return i
 
 if function_name == "run_traceroute":
     hostnames = []
@@ -16,11 +25,20 @@ if function_name == "run_traceroute":
         line = hostname_file.readline().rstrip()
     num_packets = int(sys.argv[3])
     output_filename = sys.argv[4]
-    print hostnames
+    traceroute_dict = {}
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")
+    traceroute_dict["timestamp"] = timestamp
+    print traceroute_dict["timestamp"]
     for name in hostnames:
         ls_output, err = subprocess.Popen(["traceroute", "-A", "-q", str(num_packets), name], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
         ls_output_lines = ls_output.splitlines()
-        print ls_output_lines[1][0]
+        for line in ls_output_lines:
+            line = line.split()
+            if line[0] == "traceroute":
+                continue
+            if find_num_names(line) == 0:
+
+
 
 elif function_name == "parse_traceroute":
     pass
