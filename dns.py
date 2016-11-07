b79e370920a7e709f4abdc397314ba9034e87f8b
Sukyun Chung <chungsukyun@berkeley.edu>
2016-11-06 23:08:36 -0800

    committed

--- dns.py
+++ dns.py
@@ -17,12 +17,16 @@ def run_dig(hostname_filename, output_filename, dns_query_server):
         line = hostname_file.readline().rstrip()
     json_list = []
     for name in hostnames:
+        name_dict = {}
+        name_dict["Name"] = name
         if dns_query_server != None:
             ls_output, err = subprocess.Popen(["dig", name, "@" + dns_query_server], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
         else:
             ls_output, err = subprocess.Popen(["dig", "+trace", "+tries=1", "+nofail", name], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
-        print ls_output
-        
+            name_dict["Success"] = True
+            query_list = []
+            time_lines = ls_output.split(";;")
+            print time_lines[0]
 
 if function_name == "run_dig":
     hostname_filename = sys.argv[2]
