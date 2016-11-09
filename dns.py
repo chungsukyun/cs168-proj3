94bd1fbaabf52a5d3acf39254c48b9ce2cb30367
Sukyun Chung <chungsukyun@berkeley.edu>
2016-11-08 17:26:31 -0800

    committed

--- dns.py
+++ dns.py
@@ -27,7 +27,7 @@ def run_dig(hostname_filename, output_filename, dns_query_server):
                 query_list = []
                 for query in queries:
                     query_dict = {}
-                    query_dict["Time in millis"] = int(query.splitlines()[-5].split()[2])
+                    print query.splitlines()[-5].split()[2]
                     query = query.split(";;")[6:]
                     ans_list = []
                     for section in query:
