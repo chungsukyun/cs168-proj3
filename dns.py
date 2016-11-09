af843dd0504cc7ed542b89990d0a3eb4ef16b159
Sukyun Chung <chungsukyun@berkeley.edu>
2016-11-08 17:14:30 -0800

    committed

--- dns.py
+++ dns.py
@@ -23,7 +23,7 @@ def run_dig(hostname_filename, output_filename, dns_query_server):
             if dns_query_server != None:
                 ls_output, err = subprocess.Popen(["dig", name, "@" + dns_query_server], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
                 queries = ls_output.split("; <")[1:]
-                print query
+                print queries
                 for query in queries:
                     pass
             else:
