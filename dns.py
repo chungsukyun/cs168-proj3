7f52b5555272a319cd57761dfbf0a54586fc616d
Sukyun Chung <chungsukyun@berkeley.edu>
2016-11-09 23:52:38 -0800

    committed

--- dns.py
+++ dns.py
@@ -8,6 +8,15 @@ import datetime
 
 function_name = sys.argv[1]
 
+def dot_count(string):
+    i = 0
+    count = 0
+    while i < len(string):
+        if string[i] == ".":
+            count += 1
+        i += 1
+    return count
+
 def run_dig(hostname_filename, output_filename, dns_query_server):
     hostnames = []
     hostname_file = open(hostname_filename, 'r')
@@ -51,7 +60,6 @@ def run_dig(hostname_filename, output_filename, dns_query_server):
                 name_dict["Success"] = True
                 query_list = []
                 time_lines = ls_output.split(";;")
-                print ls_output
                 i = 1
                 while i < len(time_lines) - 1:
                     query_dict = {}
@@ -81,10 +89,74 @@ def get_average_ttls(filename):
     f = open(filename, "r")
     f_str = f.read()
     f_list = json.loads(f_str)
+    root_list = []
+    tld_list = []
+    other_name_list = []
+    terminating_list = []
+    rl = 0
+    tld = 0
+    onl = 0
+    tl = 0
     for dig in f_list:
+        root_list_1 = []
+        tld_list_1 = []
+        other_name_list_1 = []
+        terminating_list_1 = []
         for query in dig["Queries"]:
+            root_list_2 = []
+            tld_list_2 = []
+            other_name_list_2 = []
+            terminating_list_2 = []
             for answer in query["Answers"]:
-                pass
+                if answer["Type"] == "A" or answer["Type"] == "CNAME":
+                    terminating_list_2 += [answer["TTL"]]
+                if answer["Queried name"] == ".":
+                    root_list_2 += [answer["TTL"]]
+                elif dot_count(answer["Queried name"]) == 1:
+                    tld_list_2 += [answer["TTL"]]
+                elif answer["Type"] == "NS":
+                    other_name_list_2 += [answer["TTL"]]
+            rl2 = 0
+            tld2 = 0
+            onl2 = 0
+            tl2 = 0
+            if len(root_list_2) != 0:
+                rld2 = numpy.mean(root_list_2)
+                root_list_1 += [rld2]
+            if len(tld_list_2) != 0:
+                tld2 = numpy.mean(tld_list_2)
+                tld_list_1 += [tld2]
+            if len(other_name_list_2) != 0:
+                onl2 = numpy.mean(other_name_list_2)
+                other_name_list_1 += [onl2]
+            if len(terminating_list_2) != 0:
+                tl2 = numpy.mean(terminating_list_2)
+                terminating_list_1 += [tl2]
+        rl1 = 0
+        tld1 = 0
+        onl1 = 0
+        tl1 = 0
+        if len(root_list_1) != 0:
+            rld1 = numpy.mean(root_list_1)
+            root_list += [rld1]
+        if len(tld_list_1) != 0:
+            tld1 = numpy.mean(tld_list_1)
+            tld_list += [tld1]
+        if len(other_name_list_1) != 0:
+            onl1 = numpy.mean(other_name_list_1)
+            other_name_list += [onl1]
+        if len(terminating_list_1) != 0:
+            tl1 = numpy.mean(terminating_list_1)
+            terminating_list += [tl1]
+    if len(root_list) != 0:
+        rld = numpy.mean(root_list)
+    if len(tld_list) != 0:
+        tld = numpy.mean(tld_list)
+    if len(other_name_list) != 0:
+        onl = numpy.mean(other_name_list)
+    if len(terminating_list) != 0:
+        tl = numpy.mean(terminating_list)
+    return [rld, tld, onl, tl]
 
 
 if function_name == "run_dig":
