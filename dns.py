5678b6ea26c1a18bad8e2304e3ec48bb695249fd
Sukyun Chung <chungsukyun@berkeley.edu>
2016-11-11 10:41:05 -0800

    committed

--- dns.py
+++ dns.py
@@ -228,7 +228,7 @@ def generate_time_cdfs(json_filename, output_filename):
         y_list2[i] = float(y_list2[i])/float(len(terminating_list))
     plot.plot(whole_list, y_list1, label="total time cdf")
     plot.plot(terminating_list, y_list2, label="final request time cdf")
-    plot.LogScale(x)
+    plot.xscale("log")
     plot.legend()
     plot.grid()
     plot.xlabel("time in millis")
