484d21fb1e59128d916f1cc9b89f4cbd37e0ad60
Sukyun Chung <chungsukyun@berkeley.edu>
2016-11-10 18:58:40 -0800

    committed

--- dns.py
+++ dns.py
@@ -280,20 +280,18 @@ def count_different_dns_responses(filename1, filename2):
             counter.add(frozenset(s))
         if len(counter) > 1:
             f1_diff += 1
-    print f1_dict
     whole_dict = f1_dict.copy()
     for hostname in f2_dict:
         if hostname not in whole_dict.keys():
             whole_dict[hostname] = f2_dict[hostname]
         else:
-            whole_dict[hostname] += [f2_dict[hostname]]
-    print whole_dict
-    # for hostname in whole_dict:
-    #     counter = set()
-    #     for s in whole_dict[hostname]:
-    #         counter.add(frozenset(s))
-    #     if len(counter) > 1:
-    #         whole_diff += 1
+            whole_dict[hostname] += f2_dict[hostname]
+    for hostname in whole_dict:
+        counter = set()
+        for s in whole_dict[hostname]:
+            counter.add(frozenset(s))
+        if len(counter) > 1:
+            whole_diff += 1
 
 
 
